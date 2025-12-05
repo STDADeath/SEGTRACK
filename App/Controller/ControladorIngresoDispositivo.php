5aa6b6795f7af4fff314dfccf0ed6512a15f9cae
Andres Carrillo <carrimorro9080@gmail.com>
2025-12-05 15:09:41 -0500

    Implementacion de Lector de qr Dispositivo v4

--- App/Controller/ControladorIngresoDispositivo.php
+++ App/Controller/ControladorIngresoDispositivo.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once __DIR__ . "/../Model/ModeloDispositivo.php";
+require_once __DIR__ . "/../Model/ModeloIngresoDispositivo.php";
 
 class ControladorDispositivo {
     private $modelo;
