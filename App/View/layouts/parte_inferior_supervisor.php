df0574c6e767614629ca6b4273d616a8fefcd4e9
Martin <jmartinml0203@gmail.com>
2026-03-20 20:02:35 -0500

    ajustes en la main por fallal en actualizar dispositivos y vehiculos de momento no se actualiza el qr pero si los datos en bd

--- App/View/layouts/parte_inferior_supervisor.php
+++ App/View/layouts/parte_inferior_supervisor.php
@@ -1,9 +1,7 @@
-        </div>
+</div>
         <!-- Fin container -->
-
     </div>
     <!-- Fin contenido -->
-
     <!-- ==============================
     📌 FOOTER
     ============================== -->
@@ -14,60 +12,61 @@
             </div>
         </div>
     </footer>
-
 </div>
 </div>
-
 <!-- ==============================
 🔝 BOTÓN SUBIR
 ============================== -->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
-
 <!-- ==============================
 🔐 MODAL LOGOUT (FUNCIONAL)
 ============================== -->
 <div class="modal fade" id="logoutModal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
-
-            <!-- Header -->
             <div class="modal-header">
                 <h5 class="modal-title">Cerrar sesión</h5>
                 <button class="close" data-dismiss="modal">
                     <span>&times;</span>
                 </button>
             </div>
-
-            <!-- Body -->
             <div class="modal-body">
                 ¿Seguro que deseas cerrar sesión?
             </div>
-
-            <!-- Footer -->
             <div class="modal-footer">
                 <button class="btn btn-secondary" data-dismiss="modal">
                     Cancelar
                 </button>
-
-                <!-- 🔥 ESTE ES EL QUE FUNCIONA -->
                 <a class="btn btn-primary" href="../Login/logout.php">
                     Cerrar sesión
                 </a>
             </div>
-
         </div>
     </div>
 </div>
-
 <!-- ==============================
-📦 SCRIPTS (SOLO UNA VEZ)
+📦 SCRIPTS (ORDEN CORRECTO)
 ============================== -->
+<!-- 1. jQuery -->
 <script src="../../../Public/vendor/jquery/jquery.min.js"></script>
+
+<!-- 2. Bootstrap -->
 <script src="../../../Public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- 3. jQuery Easing -->
 <script src="../../../Public/vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="../../../Public/js/sb-admin-2.min.js"></script>
+
+<!-- 4. DataTables (AGREGADOS) -->
+<script src="../../../Public/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="../../../Public/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- 5. SweetAlert2 (AGREGADO) -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- 6. sb-admin-2 -->
+<script src="../../../Public/js/javascript/demo/sb-admin-2.min.js"></script>
 
 </body>
 </html>
